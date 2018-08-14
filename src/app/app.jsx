321a58f043a46e54b6a0af6f1d47b7cecba897fb
Jesus Gómez <jesus_jgb_45@hotmail.com>
2018-08-14 18:31:47 +0200

    feat: add firebase & firebase auth

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import firebase from 'firebase/app';
 
 const Text = styled.p`
   font-family: sans-serif;
@@ -13,10 +14,21 @@ const Text = styled.p`
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
   width: 100%;
   min-height: 100vh;
   background-color: lightyellow;
 `;
+
+const Button = styled.button`
+  height: 64px;
+  width: 20%;
+`;
+
+function register() {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
+}
 
 export const App = () => (
   <Container>
@@ -24,5 +36,6 @@ export const App = () => (
       <i className="material-icons">info_outline</i>
       Hello World
     </Text>
+    <Button onClick={register}>Google Sign In</Button>
   </Container>
 );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 /* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
 import { App } from './app/app';
+import { firebaseConfig } from './firebase';
 import './assets/reset.css';
 
+firebase.initializeApp(firebaseConfig);
 ReactDOM.render(<App />, document.getElementById('app'));
